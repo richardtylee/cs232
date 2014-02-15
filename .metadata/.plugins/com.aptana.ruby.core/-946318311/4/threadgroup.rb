46f50da174460b4a32249718b8c55d525dcdd98c
richardtylee <richardtylee@gmail.com>
2014-02-15 02:02:52 -0800

    Add welcome page.

---
+++ .metadata/.plugins/com.aptana.ruby.core/-946318311/4/threadgroup.rb
@@ -0,0 +1,24 @@
+class ThreadGroup < Object
+
+  Default = #<ThreadGroup:0x000000015bb9d0>
+
+
+  def add(arg0)
+  end
+
+  def enclose
+  end
+
+  def enclosed?
+  end
+
+  def list
+  end
+
+
+  protected
+
+
+  private
+
+end
